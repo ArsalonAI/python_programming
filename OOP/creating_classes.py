f43c09dde33cd6762ff8d6f8871f4b00668e0055
Arsalon <arsalonamini2020@gmail.com>
2022-11-16 13:53:32 +0800

    create a class using constructor and attributes (instance data).

--- OOP/creating_classes.py
+++ OOP/creating_classes.py
@@ -1,6 +1,8 @@
 # creating a new class - use PascalCase
-# Car - a new datatype in program (attributes + behavior)
-# stored in the main file of program()
+# Car - a new datatype in program (attributes + behavior) stored in the main file of program()
+# attributes are data associated with an instance of a class
+# __init__ is constructor -> auto called upon instantiation, instance attributes set in constructor using self keyword
+
 
 class Person:
     def __init__(self, name, age, gender):
@@ -9,11 +11,18 @@ class Person:
         self.age = age
         self.gender = gender
     
-John = Person("John", 25, "Male")
+john = Person("John", 25, "Male")
 
-print(John.name,  John.age,  John.gender)
-
-
+print(john.name,  john.age,  john.gender)
 
 
 
+
+
+class Fruit: 
+    def __init__(self, name, calories):
+        self.name = name
+        self.calories = calories
+
+apple = Fruit("apple", 200)
+print(apple.calories)
--- OOP/test.py
+++ OOP/test.py
@@ -1,1 +1,2 @@
-print("this is a tes")
+print("this is a test")
+

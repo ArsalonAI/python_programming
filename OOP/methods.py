e7cb77646a1f2e5402dbd3692143d9c8f5e2d9b6
Arsalon <arsalonamini2020@gmail.com>
2022-11-16 14:16:57 +0800

    setters/getters in classes

--- OOP/methods.py
+++ OOP/methods.py
@@ -1,17 +1,29 @@
 
 # create a function inside class 
-# all methods require self as first parameter 
+# all instance methods require self as first parameter 
+# define all attributes in constructor, if not using yet, set to None vs. having "setter" method in class
 
 
 class Person: 
     def __init__(self, name):
         self.name = name 
+        self.age = None
 
     def say_hello(self):
         print(f"Hello, {self.name}")
+
+    # don't set attributes this way, instanciate to none in constructor to avoid error
+    def set_age (self, age):
+        self.age = age
     
+
+    def get_age(self):
+        return self.age 
+
 
 person1 = Person("arsalon")
 person1.say_hello()
+person1.set_age(25)
+print(person1.get_age())
 
 

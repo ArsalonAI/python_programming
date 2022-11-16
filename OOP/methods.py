10ed2ec8095b2e3ae50552c4385c9333a64d250f
Arsalon <arsalonamini2020@gmail.com>
2022-11-16 14:35:44 +0800

    more instance method examples

--- OOP/methods.py
+++ OOP/methods.py
@@ -59,3 +59,24 @@ while x < 10:
 
 counter1.toggle_lock()
 counter1.increment()
+
+
+class Rectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    
+
+    def change_position(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_position(self):
+        return self.x, self.y
+
+    def get_area(self):
+        return self.width * self.height
+
+        

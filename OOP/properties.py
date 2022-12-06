e5d72a134f67a2ae0462bcaa51360738acc939aa
Arsalon <arsalonamini2020@gmail.com>
2022-12-06 16:00:30 +0800

    more property examples

--- OOP/properties.py
+++ OOP/properties.py
@@ -56,3 +56,24 @@ p.networth = 1000000
 print(p.name, p.networth)
 
 
+#using getter/setter functions 
+
+class BankAccount:
+    def __init__(self, account_holder_name):
+        self.account_holder_name = account_holder_name
+        self._balance = 0
+
+    def get_balance(self):
+        return round(self._balance) 
+    
+    def set_balance(self, balance):
+        if type(balance) not in [int, float]:
+            raise TypeError("Should be a number")
+        elif balance < 0:
+            raise ValueError("Hey, invalid salary")
+        elif balance >= 100000:
+            raise ValueError("Hey, cannot exceed 100000")
+
+        self._balance = balance 
+
+# using properties

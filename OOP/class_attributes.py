666098b003b5c0ebf08fdbfe480b66765d1a1eda
Arsalon <arsalonamini2020@gmail.com>
2022-12-07 20:24:35 +0800

    class methods / attributes extended examples

--- OOP/class_attributes.py
+++ OOP/class_attributes.py
@@ -34,3 +34,69 @@ class Circle:
         return cls.area(radius), cls.circumference(radius)
     
 print(Circle.get_area_and_circumference(4))
+
+
+
+class Person:
+    population = 0
+  
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.population = 1
+        Person.population += 1
+      
+    @classmethod
+    def reduce_population(cls, reduction):
+        cls.population -= reduction
+    
+
+p1 = Person("RC", 100)
+p2 = Person("RC's Friend", 54)
+
+x = Person.population
+print(x)
+
+
+
+class Employee:
+    number_of_employees = 0
+    average_age = 0
+    average_salary = 0
+    
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary 
+        
+        
+        total_age = Employee.average_age * Employee.number_of_employees
+        total_salary = Employee.average_salary * Employee.number_of_employees
+        Employee.average_age = (total_age + age) / (Employee.number_of_employees + 1)
+        Employee.average_salary = (total_salary + salary) / (Employee.number_of_employees + 1)
+        Employee.number_of_employees += 1
+ 
+ 
+class Temperature:
+    min_temperature = 0
+    max_temperature = 1000
+     
+    def __init__(self, kelvin):
+        if kelvin > self.max_temperature or kelvin < self.min_temperature:
+            raise Exception("Invalid temperature")
+        self.kelvin = kelvin 
+        
+    @classmethod 
+    def update_min_temperature(cls, kelvin):
+        if kelvin > cls.max_temperature:
+            raise Exception("Invalid Temp")
+        
+        cls.min_temperature = kelvin
+        
+        
+    @classmethod
+    def update_max_temperature(cls, kelvin):
+        if kelvin < cls.min_temperature:
+            raise Exception("Invalid temperature.")
+
+        cls.max_temperature = kelvin

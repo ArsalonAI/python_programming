efa833c3330d99960e9487a27d1bf7d3344a6135
Arsalon <arsalonamini2020@gmail.com>
2022-12-10 16:19:34 +0800

    Interfaces extended excercise

--- interfaces.py
+++ interfaces.py
@@ -28,3 +28,43 @@ go = GoCode()
 java = JavaCode()
 run_code(java)
 run_code(go)
+
+
+import math
+
+
+class ShapeInterface:
+    def get_area(self):
+        raise NotImplementedError()
+
+    def get_perimeter(self):
+        raise NotImplementedError()
+
+
+class Square(ShapeInterface):
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    def get_area(self):
+        return self.side_length ** 2
+        
+    def get_perimeter(self):
+        return 4 * (self.side_length)
+
+class Circle(ShapeInterface):
+    def __init__(self, radius):
+        self.radius = radius
+        
+    def get_area(self,):
+        return math.pi * (self.radius ** 2)
+        
+    def get_perimeter(self):
+        return 2 * math.pi * self.radius
+
+c = Circle(3)
+print(c.get_area())
+print(c.get_perimeter())
+
+s = Square(2)
+print(s.get_area())
+print(s.get_perimeter())

0f4a157db66b99e0646488587685a36f1cc98386
Arsalon <arsalonamini2020@gmail.com>
2022-12-10 17:50:32 +0800

    Packages - importing modules (python files and functions from those files):

---
+++ Advanced/modules_main.py
@@ -0,0 +1,11 @@
+import my_package
+from my_package.functions import foo, bar, MyClass
+
+foo()
+bar()
+print(MyClass())
+
+
+
+
+
---
+++ Advanced/my_package/__init__.py
@@ -0,0 +1,4 @@
+# runs everytime you import the package
+import my_package.a
+import my_package.b
+import my_package.functions

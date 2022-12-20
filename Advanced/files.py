bb74bbad9955423a5f48b3a602aefc0c81af66fc
Arsalon <arsalonamini2020@gmail.com>
2022-12-20 20:31:50 +0800

    Files extended excercise

--- Advanced/files.py
+++ Advanced/files.py
@@ -33,3 +33,18 @@ with open("Advanced/file4.txt", "r+") as file:
         print(line, end="")
         if i == 2:
             break
+        
+        
+text = ""
+
+with open("Advanced/file4.txt", "r") as file:
+    text = file.read()
+
+escape_characters_removed = text.replace("\n", "")
+punctuation_removed = escape_characters_removed.replace(",", "").replace("-", "").replace(".","")
+
+words = punctuation_removed.split(" ")
+empty_strings = words.count("")
+number_of_words = len(words) - empty_strings
+
+print(number_of_words)

fe92a5c9677fdd4856c25be8403d7a2c5817f6c6
Arsalon <arsalonamini2020@gmail.com>
2022-12-20 19:54:31 +0800

    Stripping spaces from files

--- Advanced/files.py
+++ Advanced/files.py
@@ -1,3 +1,15 @@
-file = open("Advanced/file.txt", "r") # open in read mode 
-print(file.read())
-file.close()
+"""
+    file = open("Advanced/file.txt", "r") # open in read mode 
+    print(file.read())
+    file.close()
+"""
+
+#with statements provides cleanup operations (open/close)
+
+with open("Advanced/file.txt", "r") as file:
+    line1 = file.readlines()[0] # gives us each line in file, access first line
+    print([line1.strip()]) # removes any /n from our text
+    
+
+
+

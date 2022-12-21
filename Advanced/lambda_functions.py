20f01aa2619048744383a2de906577ef0e83a1d8
Arsalon <arsalonamini2020@gmail.com>
2022-12-21 17:57:06 +0800

    lambdas extended examples - returning functions from lambdas, chaining function calls

---
+++ Advanced/lambda_functions.py
@@ -0,0 +1,31 @@
+# annonymous, one line functions defined where they're used 
+# use "lambda" keyword to right is parameters
+# after colon ':' it's the function body (evaluated and returned) from fn
+
+func = lambda x, y, z = 0: print(x + y + z)
+func(1, 2)
+
+
+# named function - selects the value from a kv pair as the sort parameter
+def sort_func(x):
+    return x[1] 
+
+lst = [(1,2), (-3, 5), (0, 0), (5, 8)]
+lst.sort(key=sort_func)
+print(lst)
+
+
+# use lambda 
+lst_two = [(1,2), (-3, 5), (0, 0), (5, 8)]
+lst_two.sort(key=lambda x: x[1])
+print(lst_two)
+
+# return lambda from a lambda
+mul = lambda x: lambda y: x * y
+
+result = mul(2) # we have another function in result variable (first fn returns a fn)
+print(result(4)) # call the second fn and pass the final arg
+
+result_two = mul(4)(4) # chain fn calls, whatever fn is returned w/ first arg, second arg is passed and second fn called
+print(result_two)
+

511e7435f1576b4bb329fae71d7764b586b95376
Arsalon <arsalonamini2020@gmail.com>
2022-12-21 16:51:42 +0800

    kwargs and args extended excercise:

--- Advanced/args_kwargs.py
+++ Advanced/args_kwargs.py
@@ -1,5 +1,6 @@
 
-# *args - allows us to pass any number of arguments to a function
+# *args as parameter 
+# allows us to pass any number of arguments to a function
 # stores all args in a tuple as they appear
 
 def sum_items(*args):
@@ -9,28 +10,57 @@ sum_items(1, 2, 3)
 sum_items(2, 3, 4, 5, 6, 7)
 
 
-# **kwargs allows us to pass keyword arguments - stores in a dictionary
+# **kwargs as parameter
+# allows us to pass keyword arguments - stores in a dictionary (k-v pairs)
 def print_items(*args, **kwargs):
     print(args)
     print(kwargs)
     
 print_items(1, 2, a = 3, b = "string", c = 9)
 
-# pass elements in a datastructure to a function as parameters 
+
+# *args and **kwargs as args (unpacking operators) - splat operator - one star '*' unpacks a string, list, etc
 def take_items(a, b, c):
     return a + b + c 
 
-items = [4, 5, 6]
+items = [4, 5, 6] # list 
+new_items = {"a" : 10, "b" : 15, "c" : 24} # dictionary 
 
-# this becomes inefficient with many argument
-x = take_items(items[0], items[1], items[2]) 
-print(x)
+print(take_items(*items)) # separates the positional datastructure (collection), passing each element one at a time to the fn as parameters
+print(take_items(**new_items)) # double '**' unpacks a dictionary - allows to use dictionary as arg to function 
 
-# splat operator - one star '*' unpacks a list
-y = take_items(*items) # separates the positional datastructure (collection), passing each element one at a time to the fn as parameters
-print(y)
 
-new_items = {"a" : 10, "b" : 15, "c" : 24}
+# we can use *args and **kwargs as splat operator args (together) 
+demo_list = [1, 2,]
+demo_dict = {"a": 5, "b": 10, "c": 15}
 
-z = take_items(**new_items) # double '**' unpacks a dictionary
-print(z)
+def splat_demo(p1, p2, a=None, b=None, c=None):
+    print(p1, p2, a, b, c)
+    return a + b + c + p1 + p2
+
+x_y = splat_demo(*demo_list, **demo_dict)
+print(x_y)
+
+
+#using *args and **kwargs as args to unpack and print lists/dicts easily
+values = [1, 2, 4, 5, 6, 7, 8, 9]
+print(*values) # splits everything into individual arguments and pass them to fn
+
+list_values = [1, 2, 3, 4]
+map_values = {'end': "|", 'sep': "*"} # unpacks to end = | and sep = * (print fn accepts these params to give separator and end of print statment)
+print(*list_values, **map_values)
+
+
+# Excercise - write a fn that accepts unlimited num of positional and keyword args
+# Return True if there are at least 4 args and keyword arg "num" exists and is a number larger than 5
+# otherwise return false 
+def get_args_and_kwargs(*args, **kwargs):
+    num_args = len(args) + len(kwargs)
+    num = kwargs.get("num", 0)
+    
+    if not isinstance(num, int) and not isinstance(num, float):
+       return False
+
+    return num_args >= 4 and num > 5
+
+print(get_args_and_kwargs("a", [2], 3, num=6))

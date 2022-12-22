640475d2595de49c4fdb83707469a233da5b1ab1
Arsalon <arsalonamini2020@gmail.com>
2022-12-22 16:58:38 +0800

    using the map and filter functions together

--- Advanced/map_filter.py
+++ Advanced/map_filter.py
@@ -1,7 +1,5 @@
 import math
 # Map -> map all values in an iterable object (collection) to a new value
-# Filter -> filter for specific values in an iterable object
-
 lst = [1, 2, 3, 4, 5, 6, 7]
 
 new_lst = map(lambda x: x**2, lst)  # map -> takes a fn (lambda) and iterable obj -> return iterable obj
@@ -18,8 +16,11 @@ nested_list = [[1, 2, 6] , [3, 4, 8, 9] , [5, 6, 7]]
 nested_sum = list(map(lambda x: sum(x), nested_list)) # each arg is a list, returns a list of sum of elements
 print('the sum of the nested list is', nested_sum)
 
-
-# Filter - gives values that satisify a specific constraint - pass fn that returns T/F (specifies to keep item or skip)
+# Filter - filter for specific values in an iterable object | gives values that satisify a specific constraint - pass fn that returns T/F (specifies to keep item or skip)
 filter_list = filter(lambda x: sum(x) > 10, nested_list) # only keep lists with elements that sum greater than 10
 for el in filter_list:
     print('this sublist is greater than 10', el)
+    
+# Using Map and Filter together - map values to sums
+map_filter_list = filter(lambda y: y % 2 == 0, map(lambda x: sum(x), nested_list)) #map returns a collection of sums, filter selects those sums that are even nums
+print(list(map_filter_list))

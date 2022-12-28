975e00fd4556041f0bf3824f11adfb9afbe54fba
Arsalon <arsalonamini2020@gmail.com>
2022-12-28 16:14:12 +0800

    Iterators extended example

--- Advanced/iterators.py
+++ Advanced/iterators.py
@@ -51,8 +51,30 @@ for num in nums:
     print(num)
     
        
-    
-    
-        
+# Write a custom class that contains an iterator    
+class Range:
+    def __init__(self, start, stop, step):
+        self.start = start
+        self.stop = stop
+        self.step = step
 
+    def __iter__(self):
+        self.current_value = self.start
+        return self 
+
+    def __next__(self):
+        if self.step > 0 and self.current_value >= self.stop:
+            raise StopIteration
+        elif self.step < 0 and self.current_value <= self.stop:
+            raise StopIteration
+
+        self.current_value += self.step 
+
+        return self.current_value - self.step 
+
+range_enumerator = Range(1, 30, 2)
+
+for val in range_enumerator:
+    print(val)
+    
 

671105b9d0b92dd4ff689f868154471d54f3d4a5
Arsalon <arsalonamini2020@gmail.com>
2022-12-29 14:58:54 +0800

    Thread yielding control example - when a thread is hanging, the alternative thread can take control

--- Advanced/threads.py
+++ Advanced/threads.py
@@ -1,7 +1,12 @@
 import threading 
+from time import sleep 
 
-def run(content):
+def run(content, delay=1):
+    sleep(delay)
     print(content)
     
-thread1 = threading.Thread(target=run, args=("run",))
+thread1 = threading.Thread(target=run, args=("run 1",2 ))
+thread2 = threading.Thread(target=run, args=("run 2", 1))
+
 thread1.start()
+thread2.start()
